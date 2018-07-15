bd0d0a14ab6cce587c006d63a870a2d6ebc20bab
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-07-14 22:57:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,42 +64,7 @@ client.on('guildMemberAdd', async member => {
      
 });
 
-client.on('guildMemberRemove', async member => {
-  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-  if (!channel) return;
 
-  const canvas = Canvas.createCanvas(700, 250);
-  const ctx = canvas.getContext('2d');
-
-  const background = await Canvas.loadImage('./wallpaper.jpg');
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-  ctx.strokeStyle = '#74037b';
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-  // Slightly smaller text placed above the member's display name
-  ctx.font = '28px sans-serif';
-  ctx.fillStyle = '#ffffff';
-  ctx.fillText('Why did you leave?,', canvas.width / 2.5, canvas.height / 3.5);
-
-  // Add an exclamation point here and below
-  ctx.font = applyText(canvas, `${member.displayName}!`);
-  ctx.fillStyle = '#ffffff';
-  ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-  ctx.beginPath();
-  ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-  ctx.closePath();
-  ctx.clip();
-
-  const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
-  const avatar = await Canvas.loadImage(buffer);
-  ctx.drawImage(avatar, 25, 25, 200, 200);
-
-  const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-  channel.send(` ${member} Left the server!`, attachment);
-});
 
 client.on('message', async message => {
   if (message.content === '!le') {
